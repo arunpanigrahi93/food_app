8de3128cdc305b593e1066ae56f4fbb9105d3522
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-08-21 16:45:04 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" className="logo" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.{" "}
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Delivery</li>
+          <li>Privacy policy</li>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-123-456-234</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import "./Header.css";
+import background from "../../assets/header_img.png";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        {/* <h2>Order your favouritr food here</h2>
+        <p>Choose from a wide variety of dishes</p> */}
+        <img
+          src={background}
+          alt="Description of the image"
+          className="header-image"
+        />
+        {/* <button>View Menu</button> */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
