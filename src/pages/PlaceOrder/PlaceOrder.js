bacecc66bdf7da904006e75a0a21f603c8d5b922
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2024-08-22 08:14:44 +0530

    fixed the placeorder checkout amount issue

--- src/pages/PlaceOrder/PlaceOrder.js
+++ src/pages/PlaceOrder/PlaceOrder.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./PlaceOrder.css";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 const PlaceOrder = () => {
-  // Using useSelector to get cartItems and food_list from the Redux store
   const cartItems = useSelector((state) => state.cart.cartItems);
   const foodList = useSelector((state) => state.cart.food_list);
 
@@ -14,6 +14,11 @@ const PlaceOrder = () => {
       return total + itemInfo.price * cartItems[itemId];
     }, 0);
   };
+
+  const location = useLocation();
+  const originalTotal = getTotalCartAmount() + 2;
+  const totalAfterDiscount = location.state?.total || originalTotal;
+  const discount = originalTotal - totalAfterDiscount;
 
   return (
     <form className="place-order">
@@ -41,19 +46,22 @@ const PlaceOrder = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>${(originalTotal - 2).toFixed(2)}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Discount</p>
+              <p>-${discount.toFixed(2)}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+              <p>${originalTotal === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>
-                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
-              </b>
+              <b>${totalAfterDiscount.toFixed(2)}</b>
             </div>
           </div>
           <button>PROCEED TO PAYMENT</button>
